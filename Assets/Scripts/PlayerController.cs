b0b36a6a88fd03fc99513be7abcd6cdea235616f
CodeNerd20 <EthanSchlosser19@gmail.com>
2023-11-28 10:30:16 -0800

    Tuesday Work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5f;
+    private Rigidbody rb;
+    private Animator playerAnim;
+    private bool gameOver = false;
+    public Vector3 direction = Vector3.forward;
+
+    private float horizontalInput;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        if(horizontalInput == 1)
+        {
+            direction = Vector3.right;
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ghost"))
+        {
+            gameOver = true;
+            playerAnim.SetBool("Death", true);
+        }
     }
 }
